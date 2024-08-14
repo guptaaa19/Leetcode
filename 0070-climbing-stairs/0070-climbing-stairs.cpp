a9285fdb117a24008b03b5997305d4ec58f4e6f8
guptaaa19 <118473640+guptaaa19@users.noreply.github.com>
2024-08-14 19:54:07 +0530

    Time: 2 ms (49.26%), Space: 7.4 MB (44.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[46];
-        // int dp[i] = dp[n+1];
+        int dp[n+1];
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
-        for(int i =3;i<=n;++i){
+        for(int i =2;i<=n;++i){
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
