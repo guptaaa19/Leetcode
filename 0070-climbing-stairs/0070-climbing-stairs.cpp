8b3ff1a559aee5c99d544a7456fdfd17a007a16e
guptaaa19 <118473640+guptaaa19@users.noreply.github.com>
2024-08-16 01:42:14 +0530

    Time: 2 ms (49.11%), Space: 7.5 MB (45.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,15 @@
+vector<int>dp(46, -1);
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i =2;i<=n;++i){
-            dp[i] = dp[i-1] + dp[i-2];
+        if(dp[n] != -1){
+            return dp[n];
         }
-        return dp[n];
+        if(n<=2){
+            return dp[n] = n;
+        }
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
     }
 };
+
+// Recursive Solution turned into dp
