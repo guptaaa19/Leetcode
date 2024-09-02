8b0b79af3572b0cd6322dd3129c8adcdc6103a82
guptaaa19 <118473640+guptaaa19@users.noreply.github.com>
2024-09-03 00:30:43 +0530

    Time: 95 ms (62.02%), Space: 83.7 MB (5.65%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -3,16 +3,27 @@ public:
     int chalkReplacer(vector<int>& chalk, int k) {
         long long sum = 0;
         int n = chalk.size();
-        for(int i=0;i<n;i++){
-            sum+=chalk[i];
+        vector<long> prefixSum(n);
+        prefixSum[0] = chalk[0];
+        for(int i=1;i<n;i++){
+            prefixSum[i]=prefixSum[i-1]+chalk[i];
         }
-        k%=sum;
-        for (int i = 0; i < n; i++) {
-            if (k < chalk[i]) {
-                return i;
+        sum = prefixSum[n-1];
+        long remainingChalk=(k%sum);
+        return binarySearch(prefixSum, remainingChalk);
+    }
+private:
+    int binarySearch(vector<long>& arr, long tar){
+        int low = 0, high = arr.size()-1;
+        while(low<high){
+            int mid=low + (high-low)/2;
+            if(arr[mid]<=tar){
+                low = mid + 1;
             }
-            k -= chalk[i];
+            else{
+                high = mid;
+            }
         }
-        return 0;
+        return high;
     }
 };
