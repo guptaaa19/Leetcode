1b5d946e6a38bc44a932ac21f53188f04ad7f4e4
guptaaa19 <118473640+guptaaa19@users.noreply.github.com>
2024-09-10 15:52:26 +0530

    Time: 328 ms (5.08%), Space: 107.1 MB (7.82%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        map<int,int>mp;
-        for(int i: nums){
+        map<int, int> mp;
+        for (int i : nums) {
             mp[i]++;
-            if(mp[i]>1)
-            return i;
         }
-        return -1;
+        for (auto i : mp) {
+            if (i.second > 1)
+                return i.first;
+        }
+        return -1; // No duplicate found
     }
 };
