5daad56adb71576928f852bc9f6766c17482a81f
guptaaa19 <118473640+guptaaa19@users.noreply.github.com>
2025-07-23 15:12:33 +0530

    Time: 0 ms (100%), Space: 7.8 MB (14.13%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        int n = address.size();
+        string ans;
+        int i=0;
+        while(i<n){
+            if(address[i]=='.'){
+                ans += "[.]";
+                i++;
+            }else{
+                ans += address[i];
+                i++;
+            }
+        }
+        return ans;
+    }
+};
